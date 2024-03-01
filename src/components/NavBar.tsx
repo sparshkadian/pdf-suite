c12d23650ebe436c00ee497edd3187eccd9e56d5
Sparsh Kadian <sparshkadian323@gmail.com>
2024-03-01 15:18:59 +0530

    moved hero_buttons styles to css file

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,7 +4,7 @@ const NavBar = () => {
   return (
     <div className='fixed w-full h-[50px] p-2'>
       <Link to='/' className='flex items-center gap-1'>
-        <img src='./pdf.png' alt='logo' width={20} />
+        <img src='./pdf.png' alt='logo' width={15} />
         <h2 className='navbar__text'>PDF Suite</h2>
       </Link>
     </div>
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -8,19 +8,15 @@ const LandingPage = () => {
         <p className='hero__subtitle'>Do more with your PDFs and Words.</p>
         <div className='mt-4 flex gap-2'>
           <Link to='/pdfTools'>
-            <button className='relative inline-flex h-12 overflow-hidden rounded-full p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50'>
-              <span className='absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]' />
-              <span className='inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full px-3 py-1 font-medium text-black backdrop-blur-3xl'>
-                PDF Tools
-              </span>
+            <button className='hero__button'>
+              <span className='button__inner-span' />
+              <span className='button__outer-span'>PDF Tools</span>
             </button>
           </Link>
           <Link to='/wordTools'>
-            <button className='relative inline-flex h-12 overflow-hidden rounded-full p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-200'>
-              <span className='absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]' />
-              <span className='inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full text-black px-3 py-1 font-medium backdrop-blur-3xl'>
-                Word Tools
-              </span>
+            <button className='hero__button'>
+              <span className='button__inner-span' />
+              <span className='button__outer-span'>Word Tools</span>
             </button>
           </Link>
         </div>
