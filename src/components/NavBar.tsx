bd14e940c744823ab7e45e046ae76cec8d4e2a43
Sparsh Kadian <sparshkadian323@gmail.com>
2024-03-01 18:59:30 +0530

    added animation to hero content

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,7 +3,7 @@ import { Link, useLocation } from 'react-router-dom';
 const NavBar = () => {
   const location = useLocation();
   return (
-    <div className='absolute w-[50px] h-[40px] p-2 z-[1]'>
+    <div className='ml-2 mt-2 absolute w-[50px] h-[40px] p-2 z-[1]'>
       <Link to='/' className='w-full'>
         <img
           src='./home.png'
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -5,9 +5,11 @@ import { Button } from '../components/ui/moving-border';
 const LandingPage = () => {
   return (
     <LampContainer className='h-screen w-screen flex justify-center items-center'>
-      <div className='flex flex-col items-center leading-tight'>
-        <h1 className='hero__title text-white'>PDF Suite</h1>
-        <p className='hero__subtitle'>Do more with your PDFs and Words.</p>
+      <div className='hero__content flex flex-col items-center leading-tight'>
+        <h1 className='hero__title text-white google-font'>PDF Suite</h1>
+        <p className='hero__subtitle google-font'>
+          Do more with your PDFs and Words.
+        </p>
         <div className='mt-10 flex gap-4'>
           <Link to='/pdfTools'>
             <Button>PDF Tools</Button>
