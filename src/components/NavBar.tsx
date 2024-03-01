be058fa0dbaf247c0ad7ce640e62fb6e558bd5f1
Sparsh Kadian <sparshkadian323@gmail.com>
2024-03-01 15:51:46 +0530

    fixed width for navbar Link issue

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const NavBar = () => {
   return (
     <div className='absolute w-full h-[50px] p-2 z-[1]'>
-      <Link to='/' className='flex items-center gap-1'>
+      <Link to='/' className='flex items-center gap-1 w-[100px]'>
         <img src='/pdf.png' alt='logo' width={15} />
         <h2 className='navbar__text'>PDF Suite</h2>
       </Link>
--- src/components/ToolsList.tsx
+++ src/components/ToolsList.tsx
@@ -2,15 +2,21 @@ import { PdfTools } from '../constants/PDF';
 import { WordTools } from '../constants/Word';
 import ToolsItem from './ToolsItem';
 import { useParams } from 'react-router-dom';
+// import { GridBackground } from './BackgroundGrid';
 
 const ToolsList = () => {
   const params = useParams();
   return (
-    <div className='p-10 lg:max-w-7xl mx-auto'>
-      <div className='mt-6 flex flex-wrap md:flex justify-center gap-10 items-center'>
-        {params.toolType === 'pdfTools'
-          ? PdfTools.map((tool, i) => <ToolsItem key={i} tool={tool} />)
-          : WordTools.map((tool, i) => <ToolsItem key={i} tool={tool} />)}
+    <div className='relative'>
+      {/* <div className='absolute h-full w-full'>
+        <GridBackground />
+      </div> */}
+      <div className='p-10 lg:max-w-7xl mx-auto'>
+        <div className='mt-6 flex flex-wrap md:flex justify-center gap-10 items-center'>
+          {params.toolType === 'pdfTools'
+            ? PdfTools.map((tool, i) => <ToolsItem key={i} tool={tool} />)
+            : WordTools.map((tool, i) => <ToolsItem key={i} tool={tool} />)}
+        </div>
       </div>
     </div>
   );
