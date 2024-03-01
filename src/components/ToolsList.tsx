7d4e766fe156a746fba82f0e14d5f0d9fb56e71f
Sparsh Kadian <sparshkadian323@gmail.com>
2024-03-01 19:07:51 +0530

    added grid background to ToolsList component

--- src/components/ToolsList.tsx
+++ src/components/ToolsList.tsx
@@ -6,11 +6,14 @@ import { useParams } from 'react-router-dom';
 const ToolsList = () => {
   const params = useParams();
   return (
-    <div className='p-10 lg:max-w-7xl mx-auto'>
-      <div className='mt-6 flex flex-wrap md:flex justify-center gap-10 items-center'>
-        {params.toolType === 'pdfTools'
-          ? PdfTools.map((tool, i) => <ToolsItem key={i} tool={tool} />)
-          : WordTools.map((tool, i) => <ToolsItem key={i} tool={tool} />)}
+    <div className='main'>
+      <div className='gradient'></div>
+      <div className='p-10 lg:max-w-7xl mx-auto'>
+        <div className='mt-6 flex flex-wrap md:flex justify-center gap-10 items-center'>
+          {params.toolType === 'pdfTools'
+            ? PdfTools.map((tool, i) => <ToolsItem key={i} tool={tool} />)
+            : WordTools.map((tool, i) => <ToolsItem key={i} tool={tool} />)}
+        </div>
       </div>
     </div>
   );
